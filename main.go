985e6188e89dd5d74a4e403bf91b1bcc58350545
dingzebin <dingzebin0@gmail.com>
2022-08-02 18:48:43 +0800

    feat: add repl

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/user"
 
+	"github.com/bin_lang/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Bin programing language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
