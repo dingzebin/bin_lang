caaed1f7d0ae1c647b5a04dd025a103fb7787d27
dingzebin <dingzebin0@gmail.com>
2022-08-05 13:31:39 +0800

    feat: add ast

---
+++ parser/parser.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+	"github.com/bin_lang/ast"
+	"github.com/bin_lang/token"
+
+	"github.com/bin_lang/lexer"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
