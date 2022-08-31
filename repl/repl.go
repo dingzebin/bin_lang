fc1e7357f21b9179f2c7d75e7e3c710bf1079f73
dingzebin <dingzebin0@gmail.com>
2022-08-31 13:22:01 +0800

    feat: add integer and boolean literal evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/bin_lang/evaluator"
 	"github.com/bin_lang/lexer"
 	"github.com/bin_lang/parser"
 )
@@ -27,8 +28,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
