6b2c7d24783797f15847f2cf90b1f522d2df6157
dingzebin <dingzebin0@gmail.com>
2022-09-01 13:26:07 +0800

    feat: add prefix expression and infix expression evaluator

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/bin_lang/lexer"
@@ -15,6 +16,19 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}{
 		{"5", 5},
 		{"10;", 10},
+		{"-5;", -5},
+		{"-10;", -10},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50 + 100 + -50", 0},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"20 + 2 * -10", 0},
+		{"50 / 2 * 2 + 10", 60},
+		{"2 * (5 + 10)", 30},
+		{"3 * 3 * 3 + 10", 37},
+		{"3 * (3 * 3) + 10", 37},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 
 	for _, tt := range tests {
@@ -28,13 +42,32 @@ func TestEvalBooleanExpression(t *testing.T) {
 		input    string
 		expected bool
 	}{
-		{"true", true},
-		{"false", false},
+		{"(1 < 2) == true", true},
 	}
 
 	for _, tt := range tests {
 		obj := testEval(tt.input)
-		testBooleanObject(t, obj, tt.expected)
+		if !testBooleanObject(t, obj, tt.expected) {
+			fmt.Println(obj, tt.input)
+		}
+	}
+}
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
 	}
 }
 
