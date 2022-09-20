8c2bd404821dbbb3caec6581bf0f935691664c38
dingzebin <dingzebin0@gmail.com>
2022-09-20 23:13:01 +0800

    feat: add ast modifier

--- ast/ast.go
+++ ast/ast.go
@@ -97,7 +97,6 @@ func (es *ExpressionStatement) String() string {
 	return ""
 }
 
-//
 type Identifier struct {
 	Token token.Token
 	Value string
@@ -306,4 +305,23 @@ func (h *HashLiteral) String() string {
 	out.WriteString(strings.Join(pairs, ", "))
 	out.WriteString("}")
 	return out.String()
+}
+
+type ModifierFunc func(Node) Node
+
+func Modify(node Node, modifier ModifierFunc) Node {
+	switch node := node.(type) {
+	case *Program:
+		for i, statement := range node.Statements {
+			node.Statements[i], _ = Modify(statement, modifier).(Statement)
+		}
+	case *ExpressionStatement:
+		node.Expression, _ = Modify(node.Expression, modifier).(Expression)
+	case *InfixExpression:
+		node.Left, _ = Modify(node.Left, modifier).(Expression)
+		node.Right, _ = Modify(node.Right, modifier).(Expression)
+	case *PrefixExpression:
+		node.Right, _ = Modify(node.Right, modifier).(Expression)
+	}
+	return modifier(node)
 }
