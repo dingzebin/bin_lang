46287ba0a4c48b26c4ab7e97f78afd10881f1096
dingzebin <dingzebin0@gmail.com>
2022-09-26 17:10:11 +0800

    feat: macro

--- token/token.go
+++ token/token.go
@@ -50,6 +50,7 @@ const (
 	IF       = "IF"
 	ELSE     = "ELSE"
 	RETURN   = "RETURN"
+	MACRO    = "MACRO"
 )
 
 // keywords map
@@ -61,6 +62,7 @@ var keywords = map[string]TokenType{
 	"if":     IF,
 	"else":   ELSE,
 	"return": RETURN,
+	"macro":  MACRO,
 }
 
 func LookupIdent(ident string) TokenType {
